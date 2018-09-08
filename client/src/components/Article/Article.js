3d229ac359899f9041b6fd907783969721b5f689
Jason Warner <jcwarner@gmail.com>
2018-09-08 13:25:33 -0500

    This commit completes the NYT Scraper, ties in Saved Articles to MongoDB

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -6,6 +6,11 @@ const Article = ({ title, url, _id, date, handleClick, buttonText, saved }) => (
   <ListItem>
     <h3>
       <em>{title}</em>{" "}
+    </h3>
+    <p>
+      Date {saved ? "Saved" : "Published"}: {formatDate(date)}
+    </p>
+    <div>
       <span className="btn-group pull-right">
         <a
           className="btn btn-light"
@@ -19,10 +24,7 @@ const Article = ({ title, url, _id, date, handleClick, buttonText, saved }) => (
           {buttonText}
         </button>
       </span>
-    </h3>
-    <p>
-      Date {saved ? "Saved" : "Published"}: {formatDate(date)}
-    </p>
+    </div>
   </ListItem>
 );
 
